2771208a998da6f6898eb57f7069c058244295e2
marcinu456 <marcinu456@gmail.com>
2023-11-20 00:04:03 +0100

    Indicators trying to understand

---
+++ src/Assignments/Uniforms/app.h
@@ -0,0 +1,27 @@
+//
+// Created by pbialas on 05.08.2020.
+//
+
+#pragma once
+
+#include <vector>
+
+#include "Application/application.h"
+#include "Application/utils.h"
+
+#include "glad/gl.h"
+
+class SimpleShapeApplication : public xe::Application
+{
+public:
+    SimpleShapeApplication(int width, int height, std::string title, bool debug) : Application(width, height, title, debug) {}
+
+    void init() override;
+
+    void frame() override;
+
+    void Uniform();
+
+private:
+    GLuint vao_;
+};
---
+++ src/Assignments/Uniforms/shaders/base_fs.glsl
@@ -0,0 +1,17 @@
+#version 420
+in vec3 vColor;
+
+layout(location=0) out vec4 vFragColor;
+layout(binding = 0, std140) uniform Modifier {
+    float strength;
+    vec3  color; 
+};
+
+
+void main() {
+    
+    vFragColor = vec4(vColor*color,1);
+    vFragColor[1] = vFragColor[1]*strength;
+    vFragColor[2] = vFragColor[2]*strength;
+    vFragColor[3] = vFragColor[3]*strength;
+}
