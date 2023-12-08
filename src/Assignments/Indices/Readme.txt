d5a4270744bda4cfa98d5351ec9a0b420db13bf2
marcinu456 <marcinu456@gmail.com>
2023-12-09 00:46:00 +0100

    Poprawiono indicaty i dodano plik readme

---
+++ src/Assignments/Indices/Readme.txt
@@ -0,0 +1,5 @@
+﻿GLfloat zawiera 4 bajty
+Bez indeksów dom miał 9 wierzchołków x,y,z,r,g,b (6 wartości po 4 bajty) więc 216 bajtów
+Z indeksami dom miał 5 wierzchołków x,y,z,r,g,b (6 wartości po 4 bajty) więc 120 bajtów
+Zaoszczędziliśmy 96 bajtów
+ 
--- src/Assignments/Indices/app.cpp
+++ src/Assignments/Indices/app.cpp
@@ -39,15 +39,15 @@ void SimpleShapeApplication::init() {
 
     // A vector containing the x,y,z vertex coordinates for the triangle.
     std::vector<GLfloat> vertices = {
-        -0.5f, 0.0f, 0.0f,  0.81f,0.1f,0.1f,
-        0.5f, 0.0f, 0.0f,   0.81f,0.1f,0.1f,
-        0.0f, 0.5f, 0.0f,   0.81f,0.1f,0.1f,
-        -0.5f, 0.0f, 0.0f,  0.31f,0.1f,0.251f,
-        -0.5f, -0.5f, 0.0f, 0.31f,0.1f,0.251f,
-        0.5f, -0.5f, 0.0f,  0.31f,0.1f,0.251f,
-        0.5f,  0.0f, 0.0f,  0.31f,0.1f,0.251f};
+        0.5f, 0.0f, 0.0f,   0.1f,0.5f,0.1f,
+        0.0f, 0.5f, 0.0f,   0.1f,0.5f,0.1f,
+        -0.5f, 0.0f, 0.0f,  0.1f,0.5f,0.1f,
+        -0.5f, -0.5f, 0.0f, 0.5f,0.1f,0.1f,
+        0.5f, -0.5f, 0.0f,  0.1f,0.1f,0.5f};
 
-    std::vector<GLushort> indices = {0,1,2,3,4,5,6};
+    
+    std::vector<GLushort> indices = {0,1,2,0,2,3,3,4,0};
+    
     
     // Generating the buffer and loading the vertex data into it.
     GLuint v_buffer_handle;
@@ -60,7 +60,7 @@ void SimpleShapeApplication::init() {
     GLuint idx_buffer_handle;
     glGenBuffers(1, &idx_buffer_handle);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     
     // This setups a Vertex Array Object (VAO) that  encapsulates
@@ -77,7 +77,7 @@ void SimpleShapeApplication::init() {
     
     glEnableVertexAttribArray(1);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3*sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
     glBindVertexArray(0);
@@ -101,7 +101,7 @@ void SimpleShapeApplication::frame() {
     // Binding the VAO will setup all the required vertex buffers.
     glBindVertexArray(vao_);
     //glDrawArrays(GL_TRIANGLES, 0, 9);
-    glDrawElements(GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid *>(0));
+    glDrawElements(GL_TRIANGLE_STRIP, 9, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid *>(0));
     glBindVertexArray(0);
-    ReadRam();
+
 }
